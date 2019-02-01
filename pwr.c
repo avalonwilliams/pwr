196d782976c2d51e05cddf4293d68ae129424234
AGitBoy <aidanwillie0317@protonmail.com>
2019-02-01 02:11:37 -0500

    Fixed the version output string

--- pwr.c
+++ pwr.c
@@ -28,7 +28,7 @@ static const char* versionstr = "pwr v1.0\n"
 				"License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>\n"
 				"This is free software: you are free to change and redistribute it.\n"
 				"There is NO WARRANTY, to the extent permitted by law.\n\n"
-				"Written by Aidan Williams\n";
+				"Written by Aidan Williams";
 
 static const char* usagestr = "Usage: %s [OPTIONS]\n\n"
 			      "-h, --help   \tPrints this help text\n"
